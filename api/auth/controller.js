d810ace3c30176a1598ec04f2f425f51f3f849a1
anandcool678 <anand201101dwivedi@gmail.com>
2022-08-01 00:17:38 +0530

    login

---
+++ api/auth/controller.js
@@ -0,0 +1,85 @@
+const ErrorResponse = require('../../util/errorResponse');
+const asyncHandler = require('../../middleware/async');
+const User = require('../user/model');
+const { generateFromEmail } = require("unique-username-generator");
+const validatePhoneNumber = require('validate-phone-number-node-js');
+
+exports.register = asyncHandler(async (req, res, next)=>{
+    const {user_Name,user_Email,user_Phone_Number, user_Password,user_Username} = req.body;
+    if(req.body.user_Username==null){
+        const t = generateFromEmail(
+            user_Email,
+            3
+        );
+        req.body.user_Username = t;
+    }
+    
+    // req.body.user_Username.replace(generate_Username); 
+    const validate_Phone_Number = validatePhoneNumber.validate(req.body.user_Phone_Number);
+    if(!validate_Phone_Number){
+        return next(new ErrorResponse('Please enter valid phone number',400));
+    } 
+    const user = await User.create({user_Name,user_Email,user_Phone_Number, user_Password,user_Username});
+
+    sendTokenResponse(user, 200, res);
+    console.log(user);
+});
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const {user_Email,user_Phone_Number, user_Password} = req.body;
+
+    const user = await User.findOne({user_Email}).select('+user_Password');
+    
+
+    // Validate email & password
+    if (!user) {
+        return next(new ErrorResponse('Invalid credewsdsfdentials', 400));
+    }
+    console.log(user);
+    const isMatch = await user.matchPassword(user_Password);
+    
+
+    if (!isMatch) {
+        console.log(user_Password);
+        return next(new ErrorResponse('Invalid password', 401));
+    }
+    // Check for user
+    sendTokenResponse(user, 200, res);
+});
+
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
+});
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Password is incorrect', 401));
+    }
+
+    user.user_Password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+    const options = {
+        expires: Number(new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000)),
+        httpOnly: true,
+        secure: true
+    };
+    res.status(statusCode).cookie('token', token, options).json({success: true, token});
+};
---
+++ api/user/model.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const userSchema = new mongoose.Schema({
+    user_Name: {
+        type: String,
+        required: [true, 'Please provide your first name'],
+    },
+    user_Email: {
+        type: String,
+        required: [true, 'Please provide an email address'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please add valid email address.'],
+    },
+    user_Phone_Number:{
+        type:Number,
+        unique:true,
+        required:[true,"Phone number required"],
+    },
+    user_Username:{
+        type:String,
+        unique:true,
+        default:null,
+        required:[false],
+    },
+    user_Password: {
+        type: String,
+        required: [true, 'Password can not be blank'],
+        select: false,
+        minlength: 8,
+        maxlength:20,
+        match: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastActive: {
+        type: Date
+    },
+    resetPasswordToken: String,
+    resetPasswordTokenExpire: Date,
+});
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('user_Password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.user_Password = await bcrypt.hash(this.user_Password, salt);
+});
+
+userSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    // eslint-disable-next-line no-return-await
+    return await bcrypt.compare(enteredPassword, this.user_Password);
+};
+userSchema.methods.matchType = async function(enterdType){
+    
+}
+module.exports = mongoose.model('User', userSchema);
